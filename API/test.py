649daf3183e499293932e40b1685de1c8ffa3539
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2025-06-12 23:43:35 -0300

    Update test.py

--- API/test.py
+++ API/test.py
@@ -1,1 +1,17 @@
-print("Olá, mundo!")
+import requests
+import json
+
+link_test = "https://webhook.site/33ed8010-98e2-424b-b2dc-6690d5c64ccc"
+
+dados = {
+    "Nome": "Ajuara",
+    "Número": 5586991095131,
+    "Mensagem:": "Olá, mundo!"
+}
+
+dados_json = json.dumps(dados)
+
+requests.post(link_test, dados_json)
+
+
+# Teste realizado através: https://webhook.site/#!/
