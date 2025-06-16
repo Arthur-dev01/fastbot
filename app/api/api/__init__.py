989f154c2b1aed746fad99d883eecfcee6ef2f37
Flavio_dev <129415592+Flavio-Davi@users.noreply.github.com>
2025-06-15 22:41:09 -0300

    Update and rename reade.md to __init__.py

---
+++ app/api/api/__init__.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class MensagemModel(BaseModel):
+    msg: str
+
+
+@app.get("/")
+def test():
+    data = {
+        "msg": "Hello, world!"
+    }
+    return data
+  
